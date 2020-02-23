d20bad3c5fdcadc871b6e6fa1c85cfb1cb0cfaae
jinglkang <45533802+jinglkang@users.noreply.github.com>
2020-02-23 15:45:58 +0800

    Create trinity.pl

---
+++ trinity.pl
@@ -0,0 +1,28 @@
+# assemble acoording gene
+#!/usr/bin/perl -w
+use File::Basename;
+use Getopt::Long;
+my $opt = GetOptions( 'input:s', \$fq_dir,
+        'output:s', \$output);
+if (!($opt && $fq_dir && $output)) {
+    print STDERR "\nExample usage:\n";
+    print STDERR "\n$0 -input=\"fastq diretory\" /
+    -output=\"trinity output direcotry\"\n\n";
+    exit;
+}
+my @results=<$fq_dir/*_results>;
+foreach my $result (@results) {
+        (my $spec)=basename($result)=~/(.*)_results/;
+        (my $out)="$output/$spec";
+        mkdir $out;
+        my @fq=<$result/*.fq>;
+        foreach my $fq (@fq) {
+                (my $gene)=basename($fq)=~/(.*)\.fq/;
+                print "$fq\n";
+                print "now is assembling $spec $gene ......\n";
+                `Trinity --seqType fq --max_memory 30G --jaccard_clip --CPU 32 --single $fq --full_cleanup --output $gene.Trinity`;
+                print "Trinity is over\n";
+                my $file=$gene.".Trinity.Trinity.fasta";
+                `mv $file $out/$gene.fasta`;
+        }
+}
